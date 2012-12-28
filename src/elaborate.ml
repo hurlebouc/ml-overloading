9a7578ecf34deab9796029d6993e2ce1f9b340de
hurlebouc <mathsmac@yahoo.fr>
2012-12-28 16:43:32 +0100

    Implémentation de l'application

--- src/elaborate.ml
+++ src/elaborate.ml
@@ -29,13 +29,13 @@ type env = {
 (* ------------------------------------------------------------------------- *)
 let rec elaborate_expr env (e : expression) : sch * expression =
   match e with
-    | EVar(x) -> 
+    | EVar(x)-> 
         (
-          try (StringMap.find x (env.vvenv)), EVar(x) with
-            | Not_found -> Error.error [Error.Expr(EVar(x))] "Erreur"
-            | _ -> Error.error [Error.Expr(EVar(x))] "Erreur"
+          try (StringMap.find x (env.vvenv)), e with
+            | Not_found -> Error.error [Error.Expr(e)] "Erreur"
+            | _ -> Error.error [Error.Expr(e)] "Erreur"
         )
-    | EFun(x, t2, m1) -> 
+    | EFun(x, t2, m1)-> 
         let nenv =
           {
             dcenv = env.dcenv; 
@@ -45,10 +45,25 @@ let rec elaborate_expr env (e : expression) : sch * expression =
           } in 
           (
             match elaborate_expr nenv m1 with
-              | (([], ([], t1)) ,n1) -> ([], ([], TArrow(t2, t1))), EFun(x, t2, n1)
-              |_ -> Error.error [Error.Expr(EFun(x, t2, m1))] "Erreur"
+              | (([],([],t1)) ,n1) -> ([],([],TArrow(t2, t1))), EFun(x, t2, n1)
+              |_ -> Error.error [Error.Expr(e)] "Erreur"
           )
-    | _ -> failwith "ELABORATION NOT IMPLEMENTED"
+    | EApp(m1, m2) -> 
+        (
+          match (elaborate_expr env m1), (elaborate_expr env m2) with
+            | (([],([],TArrow(t2, t1))), n1), (([],([],t2')), n2) when t2=t2'-> 
+                ([], ([], t1)), (EApp(n1, n2))
+            |_, _ -> Error.error [Error.Expr(e)] "Erreur"
+        )
+    | EConApp(c, lt, le) -> Error.error [Error.Expr(e)] "Not yet implemented"
+    | ELet(eps, x, m0, m1) -> Error.error [Error.Expr(e)] "Ne semble pas etre\ 
+                                explicitement typee"
+    | ELetRec(l, m2) -> Error.error [Error.Expr(e)] "Erreur"
+    | EMatch(eps, l) -> Error.error [Error.Expr(e)] "Erreur"
+    | EImplicit(t) -> Error.error [Error.Expr(e)] "Erreur"
+    | EFunI(eps, x, t, m) -> Error.error [Error.Expr(e)] "Erreur"
+    | ELam(a,m) -> Error.error [Error.Expr(e)] "Erreur"
+    | ETapp(m,t) -> Error.error [Error.Expr(e)] "Erreur"
 (* ------------------------------------------------------------------------- *)
 
 (* To conclude, apply the elaborator to the empty environment *)
