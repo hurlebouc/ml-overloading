ac6521220dc991588288e06c5ef06c2546b6d5de
hurlebouc <mathsmac@yahoo.fr>
2012-12-28 16:02:52 +0100

    début de l'implémentation de l'élaboration de termes explicites

--- src/elaborate.ml
+++ src/elaborate.ml
@@ -3,6 +3,8 @@ open Type
 open Matching
 open Exproftype
 
+module SM = StringMap
+
 (* ------------------------------------------------------------------------- *)
 
 (* The elaborator receives a term that has been alpha-converted so that
@@ -26,9 +28,27 @@ type env = {
 }
 (* ------------------------------------------------------------------------- *)
 let rec elaborate_expr env (e : expression) : sch * expression =
-
-  failwith "ELABORATION NOT IMPLEMENTED"
-
+  match e with
+    | EVar(x) -> 
+        (
+          try (StringMap.find x (env.vvenv)), EVar(x) with
+            | Not_found -> Error.error [Error.Expr(EVar(x))] "Erreur"
+            | _ -> Error.error [Error.Expr(EVar(x))] "Erreur"
+        )
+    | EFun(x, t2, m1) -> 
+        let nenv =
+          {
+            dcenv = env.dcenv; 
+            tvenv = env.tvenv;
+            vvenv = SM.add x ([], ([], t2)) (env.vvenv);
+            ivenv = env.ivenv;
+          } in 
+          (
+            match elaborate_expr nenv m1 with
+              | (([], ([], t1)) ,n1) -> ([], ([], TArrow(t2, t1))), EFun(x, t2, n1)
+              |_ -> Error.error [Error.Expr(EFun(x, t2, m1))] "Erreur"
+          )
+    | _ -> failwith "ELABORATION NOT IMPLEMENTED"
 (* ------------------------------------------------------------------------- *)
 
 (* To conclude, apply the elaborator to the empty environment *)
